fb19c8aaadb4726e793e2891a2e8ebedd70446f5
zhangyuge <j9b73rsm#HGA>
2019-05-15 18:28:45 +0800

    add: configurations

--- maskrcnn_benchmark/utils/set_gpu.py
+++ maskrcnn_benchmark/utils/set_gpu.py
@@ -5,10 +5,7 @@ import gpustat
 import random
 
 stats = gpustat.GPUStatCollection.new_query()
-ids = map(lambda gpu: int(gpu.entry['index']), stats)
-ratios = map(lambda gpu: float(gpu.entry['memory.used']) / float(gpu.entry['memory.total']), stats)
-pairs = list(zip(ids, ratios))
-random.shuffle(pairs)
-sorted_gpu = list(map(lambda t: t[0], sorted(pairs, key=lambda x: x[1])))
+ids = list(map(lambda gpu: int(gpu.entry['index']), stats))
+sorted_gpu = sorted(ids)
 
 print("GPU list: {}".format(",".join(map(str, sorted_gpu))))

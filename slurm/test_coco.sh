b229c76657407ba722d32fa0f7d73f2ea0fd0910
zhangyuge <j9b73rsm#HGA>
2019-05-15 17:24:18 +0800

    add: configurations

--- slurm/test_coco.sh
+++ slurm/test_coco.sh
@@ -4,4 +4,4 @@ export NGPUS=8
 mkdir -p logs checkpoints
 srun --partition HA_3D --mpi=pmi2 --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=COCO --kill-on-bad-exit=1 \
     python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py \
-        --config-file "configs/test_coco_multiple.yaml" 2>&1
+        --config-file "configs/test_coco_multiple.yaml" 2>&1 &

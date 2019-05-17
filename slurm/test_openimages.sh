63dcc3efe71a2fdb7cf010d3d62904c97ea650bf
zhangyuge <j9b73rsm#HGA>
2019-05-17 14:03:01 +0800

    add: test on single GPU

--- slurm/test_openimages.sh
+++ slurm/test_openimages.sh
@@ -1,7 +1,7 @@
-export NGPUS=8
 #!/usr/bin/env sh
 
+export NGPUS=8
 mkdir -p logs checkpoints
-srun --partition HA_3D --mpi=pmi2 --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=COCO --kill-on-bad-exit=1 \
+srun --partition HA_3D --mpi=pmi2 --gres=gpu:8 -n1 --job-name=COCO --kill-on-bad-exit=1 \
     python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py \
         --config-file "configs/test_openimages.yaml" 2>&1 &
---
+++ slurm/test_openimages_single.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+mkdir -p logs checkpoints
+srun --partition HA_3D --mpi=pmi2 --gres=gpu:1 -n1 --job-name=COCO --kill-on-bad-exit=1 \
+    python tools/train_net.py \
+        --config-file "configs/test_openimages_single.yaml" 2>&1 &
